6467d134aa39191665772b7ee78b152a90171d04
Silvana Zalazar <silvana.blaq@gmail.com>
2022-02-14 22:00:29 -0300

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -9,13 +9,24 @@ let $p = qs('p');
 let $body = qs('body');
 
 let username = prompt('Ingresa tu nombre');
-$h2.innertext += `${username.trim()}`;
+$h2.innerHTML += `<a href="http://google.com">${username.trim()}`;
 $h2.style.textTransform = 'uppercase';
 $astyle.color = 'green';
 
 let background = confirm('¿Desea colocar un fondo de pantalla?');
 
 background && $body.classList.add('fondo')
+
+/* for(let i=0; i <$p.length; i++){
+    if(index % 2 == 0){
+        element.classList.add('destacadoPar');
+    }else{
+        element.classList.add('destacadoImpar')
+    }
+} */
+
+
+
 
 
 $main.style.display = "block"
